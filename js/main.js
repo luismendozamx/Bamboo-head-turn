18669502c8f198bc02f9f871b06cb5d49e19b37e
Luis Mendoza <luis@mendoza.io>
2014-11-12 11:53:49 -0600

    Fixed image inside container

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready( function(){
+	image = $('#headturn');
+	container = $('#headturn-container');
+	imageWidth = image[0].clientWidth;
+	imageHeight = image[0].clientHeight;
+	console.log(imageWidth);
 
+	image.css("height", imageHeight+"px");
+	image.css("width", imageWidth+"px");
+	image.css("left", -(imageWidth/7)*3 + "px" );
+	container.css("width", (imageWidth/7) + "px");
+
+} );
